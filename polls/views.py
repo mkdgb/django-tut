b3a2821aa2b67bf889051b29a95254ea98da73f5
mkdgb <mdbaluyos@up.edu.ph>
2015-06-24 17:25:34 +0800

    second commit- (steps 3-6) modified views, tests and initial css

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Choice, Question
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the last five published questions."""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+def vote(request, question_id):
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		#redisplay the question voting form
+		return render(request, 'polls/detail.html', {
+			'question': p,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		#always return an HttpResponseRedirect after successful dealing of POST data - this prevents data from being posted twice if user hits back button.
+		#also, use POST instead of GET when dealing with data that affects server-side.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+
+
+    
